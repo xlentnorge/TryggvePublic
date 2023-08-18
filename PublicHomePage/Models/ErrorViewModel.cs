91c9cde2a2fedd899f5923da1277f52fe0abf178
Rune Mydske Nielsen <rune.mydske.nielsen@xlent.no>
2023-08-18 13:39:44 +0200

    Add empty mvc

---
+++ PublicHomePage/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PublicHomePage.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
